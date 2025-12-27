82fb94087c2cd33e69ffa533d010f6ead3f2be22
Aditya073 <adityarajendradixit@gmail.com>
2025-12-27 18:30:54 +0530

    working on home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,40 +8,101 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xfff2f2f2),
       body: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            Container(
-              margin: EdgeInsets.only(left: 10),
-              child: Column(
-                children: [
-                  Text(
-                    'Hey, Aditya',
-                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Hey, Aditya',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'Good Morning',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text(
-                    'Good Morning',
+                ),
+
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadiusGeometry.circular(20),
+                    child: Image.asset(
+                      'images/profile_image.avif',
+                      height: 70,
+                      width: 70,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            Container(
+              margin: EdgeInsets.only(right: 20, left: 20, top: 30, bottom: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                    size: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  hint: Text(
+                    'Search products',
                     style: TextStyle(
                       color: Colors.grey,
                       fontSize: 20,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                ],
+                ),
               ),
             ),
 
-            Container(
-              margin: EdgeInsets.only(right: 10),
-              child: ClipRRect(
-                borderRadius: BorderRadiusGeometry.circular(20),
-                child: Image.asset(
-                  'images/profile_image.avif',
-                  height: 70,
-                  width: 70,
-                  fit: BoxFit.cover,
-                ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  ),
+
+                  Text(
+                    'See all',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.red,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
